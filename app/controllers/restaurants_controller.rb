2f93d6e98e1490ebd4ae97f4adc2c26ebcedcc51
Samir Gossain <samir.gossain@gmail.com>
2017-02-21 16:57:12 +0000

    Step 8 Complete

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,5 +4,16 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
   end
 end
