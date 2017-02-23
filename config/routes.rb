d0d16e4afec71dbd455d8f1eee18e41f68141659
Samir Gossain <samir.gossain@gmail.com>
2017-02-23 14:58:58 +0000

    V2 stage 1 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get 'restaurants' => 'restaurants#index'
   # resources :restaurants
+
+  root "restaurants#index"
 
   resources :restaurants do
     resources :reviews
